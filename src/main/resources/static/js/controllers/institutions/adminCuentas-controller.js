75b5e4bfcd326daeae4abaca0c85f53579c76beb
jpmartinez7480 <jpmartinez7480@gmail.com>
2017-12-03 06:12:15 -0300

    vista y controlador administrar lista terminada

--- src/main/resources/static/js/controllers/institutions/adminCuentas-controller.js
+++ src/main/resources/static/js/controllers/institutions/adminCuentas-controller.js
@@ -3,20 +3,39 @@ app.controller('adminCuentasController', ['$scope', '$location','$filter','$http
     $scope.voters = [];
     $scope.associates = [];
     $scope.votacion = [];
+
     $scope.getVotersData = function() {
-        $http.get("http://localhost:9090/institutions/"+$routeParams.id+"/voters")
+        $http.get("http://localhost:9090/institutions/2/voters")
             .then(function (response) {
                 $scope.voters = response.data;
             });	
     };
 
-    $scope.mostrar = function(){
+    $scope.votantes = {voters:["votantes"]};
+
+    $scope.guardar = function(){
     	var name = document.getElementById("name").value;
     	var institution = {'id': $routeParams.id};
+    	if(name == "")
+    		alert("No ha ingresado nombre de la lista");
+    	else
+    		$scope.votacion.push(name);
     	$scope.votacion.push($scope.associates);
-    	$scope.votacion.push(name);
     	$scope.votacion.push(institution); 	
     };
+
+
+
+    $scope.limpiar = function(){
+    	$scope.associates = [];
+		$scope.voters = [];
+		$scope.getVotersData();    	
+    };
+
+    $scope.todos = function(){
+    	$scope.associates = angular.copy($scope.voters);
+    };
+
     $scope.getVotersData();
     
 }]);
