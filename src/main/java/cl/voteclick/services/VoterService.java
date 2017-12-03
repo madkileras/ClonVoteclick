85c47d8b046d5a81029e62cc0470223525f5547d
Arthur Peña Lozano <arthur.pena@usach.cl>
2017-12-03 01:12:40 -0300

    editado servicio de votante

--- src/main/java/cl/voteclick/services/VoterService.java
+++ src/main/java/cl/voteclick/services/VoterService.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin
+@RestController
+@RequestMapping("/voters")
 public class VoterService {
     @Autowired
     VoterRepository voterRepository;
