c05092c73233b23a94fd70474ffbdc66fcd96cbb
Arthur Peña Lozano <arthur.pena@usach.cl>
2017-12-03 01:11:40 -0300

    agregado servicio de votante

---
+++ src/main/java/cl/voteclick/services/VoterService.java
@@ -0,0 +1,20 @@
+package cl.voteclick.services;
+
+import cl.voteclick.model.Voter;
+import cl.voteclick.repositories.VoterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+public class VoterService {
+    @Autowired
+    VoterRepository voterRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Voter create(@RequestBody Voter resource){
+        return voterRepository.save(resource);
+    }
+
+}
