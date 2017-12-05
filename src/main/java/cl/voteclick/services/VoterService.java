a19ceb7e562bc9649ee412b1d32c87dbd27c3695
Arthur Peña Lozano <arthur.pena@usach.cl>
2017-12-05 20:43:51 -0300

    servicio de autenticacion de documento

--- src/main/java/cl/voteclick/services/VoterService.java
+++ src/main/java/cl/voteclick/services/VoterService.java
@@ -2,9 +2,21 @@ package cl.voteclick.services;
 
 import cl.voteclick.model.Voter;
 import cl.voteclick.repositories.VoterRepository;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 
 @CrossOrigin
@@ -19,6 +31,53 @@ public class VoterService {
     @ResponseBody
     public Voter create(@RequestBody Voter resource){
         return voterRepository.save(resource);
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value= "/auth")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public boolean Auth(@RequestBody HashMap<String,String> resource) throws IOException {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers()
+                    {
+                        return null;
+                    }
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                        //No need to implement.
+                    }
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                        //No need to implement.
+                    }
+                }
+        };
+
+
+        try
+        {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e)
+        {
+            System.out.println("Error de validación");
+        }
+
+        URL url = new URL("https://portal.sidiv.registrocivil.cl/usuarios-portal/pages/DocumentRequestStatus.xhtml?RUN=" + resource.get("run") +"&type=CEDULA&serial="+resource.get("ndoc"));
+        HttpsURLConnection conn= (HttpsURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd= new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line=rd.readLine())!=null){
+            if(line.contains("Vigente")){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
